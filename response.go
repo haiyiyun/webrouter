f4f3bb9116f2c000d030eeb628f6dbed2f52a70c
leetaifook <lee@leetaifook.com>
2021-11-07 11:15:26 +0800

    fix: response bug

--- response.go
+++ response.go
@@ -1,12 +1,24 @@
 package webrouter
 
 import (
+	"bytes"
 	"net/http"
+	"sync"
 )
 
 type ResponseWriter struct {
+	mu sync.RWMutex
 	http.ResponseWriter
 	wroteHeader bool
+	getResData  bool
+	data        map[string]interface{}
+	resData     []byte
+}
+
+func (rw *ResponseWriter) SetGetResData(getResData bool) {
+	rw.mu.Lock()
+	rw.getResData = getResData
+	rw.mu.Unlock()
 }
 
 func (rw *ResponseWriter) Header() http.Header {
@@ -21,5 +33,43 @@ func (rw *ResponseWriter) WriteHeader(statusCode int) {
 }
 
 func (rw *ResponseWriter) Write(b []byte) (int, error) {
+	if rw.getResData {
+		rw.mu.Lock()
+		buf := bytes.NewBuffer(rw.resData)
+		buf.Write(b)
+		rw.resData = buf.Bytes()
+		rw.mu.Unlock()
+	}
+
 	return rw.ResponseWriter.Write(b)
+}
+
+func (rw *ResponseWriter) GetResData() []byte {
+	return rw.resData
+}
+
+func (rw *ResponseWriter) initData() {
+	rw.mu.Lock()
+	if rw.data == nil {
+		rw.data = map[string]interface{}{}
+	}
+	rw.mu.Unlock()
+}
+
+func (rw *ResponseWriter) SetData(key string, value interface{}) {
+	rw.initData()
+
+	rw.mu.Lock()
+	rw.data[key] = value
+	rw.mu.Unlock()
+}
+
+func (rw *ResponseWriter) GetData(key string) (interface{}, bool) {
+	rw.initData()
+
+	rw.mu.RLock()
+	value, found := rw.data[key]
+	rw.mu.RUnlock()
+
+	return value, found
 }
